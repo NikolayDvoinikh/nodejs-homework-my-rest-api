b0e7d115c10d4aa8d76ef784c64d2e0301b72439
NikolayDvoinikh <nikolaydvoinikh@gmail.com>
2023-04-10 15:52:29 +0300

    add .env-example file and write name of vars from .env

--- config.js
+++
@@ -1,9 +0,0 @@
-const DB_HOST =
-  "mongodb+srv://Nikolay:pYSz0LAuSvmp3xSm@cluster0.pbhbwjj.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-const PORT = 3000;
-
-module.exports = {
-  DB_HOST,
-  PORT,
-};
